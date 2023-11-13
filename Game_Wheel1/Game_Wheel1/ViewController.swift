e16a760a48f65d6b911fa338302e4960a5ee7f84
tri <q01202729398@gmail.com>
2023-11-13 18:38:50 +0700

    fist commit

---
+++ Game_Wheel1/Game_Wheel1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game_Wheel1
+//
+//  Created by Admin on 13/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        accessibilityHint
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
